2b019308b3cefa7b5db1d877e4df91bc2562e5fe
Matheus Machado Faccin <96211501+MatheusMFaccin@users.noreply.github.com>
2023-05-26 08:39:59 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import linprog
+
+            
+#min -> deixa igual. max -> inverte sinal
+objetivo = [1, 2]
+coefs = [[1, 0],[1, 0],[1, 1]]
+results = [8,5,10]
+#        minX maxX minY maxY
+limites = ((5, 8), (0, None))
+    
+
+resultado  = linprog(objetivo,A_ub = coefs,b_ub = results,bounds = limites)
+
+if resultado.success:
+    x_obj, y_obj = resultado.x
+
+
+
+print("x =", x_obj, "\ny =", y_obj)
+plt.scatter(x_obj, y_obj)
+
+plt.show()
