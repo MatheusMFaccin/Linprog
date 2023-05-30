0588b967b0085e3de3e2ed33b9a5831d4de410e3
Matheus Machado Faccin <96211501+MatheusMFaccin@users.noreply.github.com>
2023-05-30 10:20:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ from scipy.optimize import linprog
 
             
 #min -> deixa igual. max -> inverte sinal
-objetivo = [1, 2]
-coefs = [[1, 0],[1, 0],[1, 1]]
-results = [8,5,10]
-#        minX maxX minY maxY
-limites = ((5, 8), (0, None))
+#para calcular o max o sinal da função objetivo tem que ser negativo 
+objetivo = [-3000, -5000]
+#lado esquerdo das equações
+coefs = [[2, 3],[4, 8]]
+#lado direito das equações
+results = [30,70]
+# minX maxX minY maxY
+#define os limites (min,max)
+limites = ((0, None))
     
-
-resultado  = linprog(objetivo,A_ub = coefs,b_ub = results,bounds = limites, method = 'simplex')
+resultado = linprog(objetivo,A_ub = coefs,b_ub = results,bounds = limites, method = 'simplex')
 
 if resultado.success:
     x_obj, y_obj = resultado.x
 
-
-
 print("x =", x_obj, "\ny =", y_obj)
+#plota os pontos x e y no grafico
 plt.scatter(x_obj, y_obj)
 
 plt.show()
